013b4b77d8e90f8b3056f705d0a4ffecf8cef4c1
Hui Wen <huiwen_28@hotmail.com>
2015-10-25 06:26:12 -0700

    Editing UI stuff for mobile platform

--- client/helpers/layout.js
+++ client/helpers/layout.js
@@ -21,8 +21,7 @@ Template.layout.events({
     console.log("clicked");
   },
    'click #userLogin':function(){
-    Router.go('userLogin'); //name of template i want to go to
+    Router.go('/'); //name of template i want to go to
     console.log("clicked");
   }
-
 });
