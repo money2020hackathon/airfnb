adea9dc9d45c64965564307a135fc8826430da40
Feng Zhonghui <fzhfzh123@gmail.com>
2015-10-25 07:50:02 -0700

    icons and logo

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.icons({
+  //ios
+  iphone: 'icons/icon-60.png',
+  iphone_2x: 'icons/icon-60@2x.png',
+  iphone_3x: 'icons/icon-60@3x.png',
+  ipad: 'icons/icon-76.png',
+  ipad_2x: 'icons/icon-76@2x.png',
+
+});
