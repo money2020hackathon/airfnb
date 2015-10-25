4abd8d20b9f00a3d41aad0fe3940c975ffe03b67
Feng Zhonghui <fzhfzh123@gmail.com>
2015-10-25 06:07:46 -0700

    nav bar refinements links to order page from side bar

--- client/helpers/layout.js
+++ client/helpers/layout.js
@@ -3,24 +3,43 @@ Template.layout.onRendered(function(){
   $(".dropdown-button").dropdown();
 });
 
+Template.layout.helpers({
+  isLoggedIn:function(){
+    if(Meteor.user()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+});
+
 Template.layout.events({
+  'click #orders':function(){
+    $('.button-collapse').sideNav('hide');
+    Router.go('ordersPage');
+  },
   'click #createOrderPage':function(){
+    $('.button-collapse').sideNav('hide');
   	Router.go('createOrderPage'); //name of template i want to go to
   	console.log("clicked");
   },
   'click #paymentPage':function(){
+    $('.button-collapse').sideNav('hide');
   	Router.go('paymentPage', {'_id':"testId"}); //name of template i want to go to
   	console.log("clicked");
   },
   'click #profilePage':function(){
+    $('.button-collapse').sideNav('hide');
   	Router.go('profilePage'); //name of template i want to go to
   	console.log("clicked");
   },
    'click #userCook':function(){
+     $('.button-collapse').sideNav('hide');
     Router.go('userCook'); //name of template i want to go to
     console.log("clicked");
   },
    'click #userLogin':function(){
+     $('.button-collapse').sideNav('hide');
     Router.go('userLogin'); //name of template i want to go to
     console.log("clicked");
   }
